e08e64d50517dfe3d674431678df94b79995a4e5
Grasmit <grasmitkatharotiya342@gmail.com>
2022-11-24 09:59:39 +0100

    auth done with jwt, started ui

---
+++ public/src/components/userPortal/login.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+function login(props) {
+  return (
+    <>
+      {/* <div classNameName="container-fluid" style={{ border: "black 1px solid" }}>
+        <div classNameName="row">
+          <div classNameName="col-6">
+            <div classNameName="row" style={{overflow:"hidden"}}>
+              <img
+                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                className="img-fluid"
+                alt="..."
+                style={{ width: "50%", height: "auto" }}
+              />
+            </div>
+          </div>
+          <div
+            classNameName="col-6"
+            style={{ color: "blue", border: "black 1px solid" }}
+          >
+            <div classNameName="row">this is for the login form</div>
+          </div>
+        </div>
+      </div>
+      <div className="container"> */}
+    <div className="row">
+        <div className="col-md-3 ">
+            <div className="row">
+                <div className="col-md-12 pink">test</div>
+                <div className="col-md-12 yellow">test</div>
+            </div>
+        </div>
+        <div className="col-md-3">
+            <div className="row">
+                <div className="col-md-12 gray">test</div>
+            </div>
+        </div>
+        <div className="col-md-6 ">
+            <div className="row">
+                <div className="col-md-12 blue">test</div>
+                <div className="col-md-6 green">test</div>
+                <div className="col-md-6 red">test</div>
+            </div>
+        </div>
+    </div>
+    <div className="row">
+        <div className="col-md-3">
+            <div className="row">
+                <div className="col-md-12 green">test</div>
+            </div>
+        </div>
+        <div className="col-md-6">
+            <div className="row">
+                <div className="col-md-12 pink">test</div>
+            </div>
+        </div>
+        <div className="col-md-3">
+            <div className="row">
+                <div className="col-md-12 gray">test</div>
+            </div>
+        </div>
+    </div> 
+{/* </div> */}
+    </>
+  );
+}
+
+export default login;
--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,10 +1,39 @@
 const User = require('../models/userModel')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
 const db = require('../db')
 
-module.exports.login = function (req,res) {
+module.exports.login = async (req,res,next) => {
 
-    res.json({"status":200,"data":"function linked ...","message":"sucessfully login function linked "})
+    const {email,password} = req.body
+
+    const user = await User.findOne({email})
+
+    if(!user) return res.json({"status":false,"data":"","message":"User does not exist"})
+
+    console.log(user)
+
+    const hashedPassword = await bcrypt.hash(password,10)
+
+    const passCheck = await bcrypt.compare(password,hashedPassword)
+    console.log(passCheck)
+    if(!passCheck){
+
+        return res.json({"status":false,"data":"","message":"Given password is wrong"})
+    }
+
+    const userData = {
+        id:Date.now(),
+        email,
+        password
+    }
+
+    jwt.sign({user}, '26gk26', (err,token)=>{
+
+        res.json({"status":200,"data":{email,"username":user.username,token},"message":"User loggen in sucessfully"})
+
+    })
 }
 
 module.exports.register = async (req,res,next) =>{
